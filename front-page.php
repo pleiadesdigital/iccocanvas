64a9cc481d044d8286cce3b1fec2237da66f8dc9
Ronaldo Ortiz <ronyortizop@yahoo.com>
2017-10-11 22:31:02 -0400

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,53 @@
+<?php /* The front page template file */ ?>
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<div id="post-<?php the_ID(); ?>" <?php post_class('pleiadesmoon-panel '); ?> >
+
+			<!-- EL PROYECTO -->
+			<section id="project" class="fpsection">
+			  <div class="fpwrap project-wrap">
+			    <h2>El Proyecto</h2>
+			    <p>Teneo, inquit, finem illi videri nihil dolere. Tria genera bonorum; Tu quidem reddes; Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Negabat igitur ullam esse artem, quae ipsa a se proficisceretur; Consequens enim est et post oritur, ut dixi. Igitur neque stultorum quisquam beatus neque sapientium non beatus.</p>
+			    <p>Nihil enim iam habes, quod ad corpus referas; Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Egone quaeris, inquit, quid sentiam? Tibi hoc incredibile, quod beatissimum. Terram, mihi crede, ea lanx et maria deprimet. Negat enim summo bono afferre incrementum diem. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. An est aliquid, quod te sua sponte delectet?</p>
+			  </div><!-- class="indie-media-wrap" -->
+			</section><!-- class="indie-media" -->
+
+			<!-- LOS MEDIOS -->
+
+			<section id="media" class="fpsection">
+			  <div class="fpwrap media-wrap">
+			    <h2>Los Medios</h2>
+			    <p>Teneo, inquit, finem illi videri nihil dolere. Tria genera bonorum; Tu quidem reddes; Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Negabat igitur ullam esse artem, quae ipsa a se proficisceretur; Consequens enim est et post oritur, ut dixi. Igitur neque stultorum quisquam beatus neque sapientium non beatus.</p>
+			    <p>Nihil enim iam habes, quod ad corpus referas; Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Egone quaeris, inquit, quid sentiam? Tibi hoc incredibile, quod beatissimum. Terram, mihi crede, ea lanx et maria deprimet. Negat enim summo bono afferre incrementum diem. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. An est aliquid, quod te sua sponte delectet?</p>
+			  </div><!-- class="indie-media-wrap" -->
+			</section><!-- class="indie-media" -->
+
+			<!-- EVENTOS -->
+
+			<section id="events" class="fpsection">
+			  <div class="fpwrap events-wrap">
+			    <h2>Eventos</h2>
+			    <p>Teneo, inquit, finem illi videri nihil dolere. Tria genera bonorum; Tu quidem reddes; Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Negabat igitur ullam esse artem, quae ipsa a se proficisceretur; Consequens enim est et post oritur, ut dixi. Igitur neque stultorum quisquam beatus neque sapientium non beatus.</p>
+			    <p>Nihil enim iam habes, quod ad corpus referas; Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Egone quaeris, inquit, quid sentiam? Tibi hoc incredibile, quod beatissimum. Terram, mihi crede, ea lanx et maria deprimet. Negat enim summo bono afferre incrementum diem. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. An est aliquid, quod te sua sponte delectet?</p>
+			  </div><!-- class="indie-media-wrap" -->
+			</section><!-- class="indie-media" -->
+
+			<!-- MODELO CANVAS    -->
+
+			<section id="canvas" class="fpsection">
+			  <div class="fpwrap canvas-wrap">
+			    <h2>Modelo Canvas</h2>
+			    <p>Teneo, inquit, finem illi videri nihil dolere. Tria genera bonorum; Tu quidem reddes; Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Negabat igitur ullam esse artem, quae ipsa a se proficisceretur; Consequens enim est et post oritur, ut dixi. Igitur neque stultorum quisquam beatus neque sapientium non beatus.</p>
+			    <p>Nihil enim iam habes, quod ad corpus referas; Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Egone quaeris, inquit, quid sentiam? Tibi hoc incredibile, quod beatissimum. Terram, mihi crede, ea lanx et maria deprimet. Negat enim summo bono afferre incrementum diem. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. An est aliquid, quod te sua sponte delectet?</p>
+			  </div><!-- class="indie-media-wrap" -->
+			</section><!-- class="indie-media" -->
+
+		</div><!-- #post-## post_class('pleiadesmoon-panel ')-->
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer();
