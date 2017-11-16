9c2bfcb3834d7124259261f5a45c29ecbd1ea2f1
RONY-DSK <ronyortiz@RONY-DSKs-iMac.local>
2017-11-15 23:32:36 -0400

    page aprendizajes done

--- template-parts/page/content-aprendizajes.php
+++ template-parts/page/content-aprendizajes.php
@@ -33,11 +33,11 @@
 			<!-- VIDEO META -->
 			<div class="video-meta">
 				<h2 class="entry-subtitle"><?php echo get_post_meta($post->ID, 'project_subtitle', true); ?></h2>
-				<?php the_excerpt(); ?>
+				<?php the_content(); ?>
 			</div>
 			<!-- VIDEO -->
 			<div class="the-video">
-				<?php the_content(); ?>
+				<?php echo get_post_meta($post->ID, 'feat-video', true); ?>
 			</div><!-- class="the-video" -->
 
 		<?php endwhile; endif; ?>
@@ -47,65 +47,66 @@
 
 	<!-- FUCK UP NIGHTS -->
 
-	<section class="entry-content funights-content">
-		<div class="funights-content-wrap">
-			<!-- CONTEXT -->
-			<div class="fun-context">
-				<?php
-				$args = array(
-					'post_type'				=> 'post',
-					'category_name'		=> 'fun-context',
-					'orderby'					=> 'date',
-					'posts_per_page'	=> 1,
-					'post_status'			=> 'publish',
-				);
-				$query = new WP_Query($args);
-				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-				?>
+	<section class="entry-content fun-content">
 
-				<!-- Title & Subtitle -->
+		<!-- CONTEXT -->
+		<div class="fun-context">
+			<div class="fun-context-wrap">
+			<?php
+			$args = array(
+				'post_type'				=> 'post',
+				'category_name'		=> 'fun-context',
+				'orderby'					=> 'date',
+				'posts_per_page'	=> 1,
+				'post_status'			=> 'publish',
+			);
+			$query = new WP_Query($args);
+			if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+			?>
+			<!-- Title & Subtitle -->
+			<?php the_title('<h2>', '</h2>'); ?>
+			<h3><?php echo get_post_meta($post->ID, 'project_subtitle', true); ?></h3>
+
+			<!-- FUN Content -->
+			<div class="columntxt">
+				<?php the_content(); ?>
+			</div><!-- class="columntxt" -->
+
+			<?php endwhile; endif; ?>
+			<?php wp_reset_postdata(); ?>
+
+			</div><!-- class="funights-context-wrap" -->
+		</div><!-- class="fun-context" -->
+
+		<!-- EVENTS -->
+
+		<div class="fun-events">
+			<div class="fun-events-wrap">
+			<?php
+			$args = array(
+				'post_type'				=> 'post',
+				'category_name'		=> 'fun-events',
+				'orderby'					=> 'date',
+				'posts_per_page'	=> 2,
+				'post_status'			=> 'publish',
+			);
+			$query = new WP_Query($args);
+			if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+			?>
+
+			<div class="event-row">
 				<?php the_title('<h2>', '</h2>'); ?>
-				<h3><?php echo get_post_meta($post->ID, 'project_subtitle', true); ?></h3>
+				<?php the_excerpt(); ?>
+				<?php the_content(); ?>
+				<div class="event-video"><?php echo get_post_meta($post->ID, 'feat-video', true); ?></div><!-- class="event-video" -->
+			</div><!-- class="event-row" -->
 
-				<!-- FUN Content -->
-				<div class="columntxt">
-					<?php the_content(); ?>
-				</div><!-- class="columntxt" -->
+			<?php endwhile; endif; ?>
+			<?php wp_reset_postdata(); ?>
 
+			</div><!-- class="fun-events-wrap"> -->
+		</div><!-- class="fun-events" -->
 
-
-
-				<?php endwhile; endif; ?>
-				<?php wp_reset_postdata(); ?>
-			</div><!-- class="fun-events" -->
-
-
-
-			<!-- EVENTS -->
-			<div class="fun-events">
-				<?php
-				$args = array(
-					'post_type'				=> 'post',
-					'category_name'		=> 'fun-events',
-					'orderby'					=> 'date',
-					'posts_per_page'	=> 2,
-					'post_status'			=> 'publish',
-				);
-				$query = new WP_Query($args);
-				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-				?>
-
-					CONTENT GOES HERE!!
-
-
-
-				<?php endwhile; endif; ?>
-				<?php wp_reset_postdata(); ?>
-			</div><!-- class="fun-events" -->
-
-
-
-		</div><!-- class="funights-content-wrap" -->
 	</section><!-- class="entry-content funights-content1" -->
 
 </div><!-- #post-## -->
